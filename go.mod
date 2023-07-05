98d74500b7811d2ae1dde804f44cbb1bc478597c
Jakub Viták <mainiak@users.noreply.github.com>
2023-07-04 20:55:12 -0700

    Serving embedded html template

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/mainiak/go-car-http
 
 go 1.20
 
-require github.com/gin-gonic/gin v1.9.1
+require (
+	github.com/gin-contrib/multitemplate v0.0.0-20230212012517-45920c92c271
+	github.com/gin-gonic/gin v1.9.1
+)
 
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
--- internal/server.go
+++ internal/server.go
@@ -2,22 +2,37 @@ package internal
 
 import (
 	"embed"
+	"net/http"
 
+	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 )
 
-//go:embed index.tpl
+//go:embed index.tmpl
 var embedFS embed.FS
 
 func Serve() {
-	data, _ := embedFS.ReadFile("index.tpl")
-	print(string(data))
+	index_tmpl, _ := embedFS.ReadFile("index.tmpl")
+
+	mr := multitemplate.NewRenderer()
+	mr.AddFromString("index", string(index_tmpl))
 
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+
+	// https://gin-gonic.com/docs/examples/html-rendering/#custom-template-renderer
+	r.HTMLRender = mr
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index", gin.H{
+			"Title": "go-car-http",
+			"H1":    "go-car-http",
+			"BodyP": "Demo go-car-http.",
 		})
 	})
+
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
